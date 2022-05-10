7b132b062723dd033074827e460662272b7e6de1
unknown <lamthuy305@gmail.com>
2022-05-10 13:58:58 +0700

    create database

---
+++ src/main/java/com/example/casemodule6/model/entity/Profile.java
@@ -0,0 +1,34 @@
+package com.example.casemodule6.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "profile")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Date birthday;
+
+    private String avatar;
+
+    private String email;
+
+    private String address;
+
+    private String phone;
+
+    @OneToOne
+    private User user;
+}
