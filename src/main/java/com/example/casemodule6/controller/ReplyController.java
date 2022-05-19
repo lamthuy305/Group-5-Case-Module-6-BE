c00c872217c4ff61e5e04325ed2a8e230ec2260c
unknown <lamthuy305@gmail.com>
2022-05-19 11:51:41 +0700

    fix hien thi sao

--- src/main/java/com/example/casemodule6/controller/ReplyController.java
+++ src/main/java/com/example/casemodule6/controller/ReplyController.java
@@ -1,14 +1,8 @@
 package com.example.casemodule6.controller;
 
-import com.example.casemodule6.model.dto.CommentForm;
 import com.example.casemodule6.model.dto.ReplyForm;
-import com.example.casemodule6.model.entity.Comment;
-import com.example.casemodule6.model.entity.NotificationDetail;
 import com.example.casemodule6.model.entity.Reply;
-import com.example.casemodule6.model.entity.StatusNotification;
-import com.example.casemodule6.service.notificationdetail.INotificationDetailService;
 import com.example.casemodule6.service.reply.ReplyService;
-import com.sun.org.apache.regexp.internal.RE;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,9 +17,6 @@ import java.util.Optional;
 public class ReplyController {
     @Autowired
     private ReplyService replyService;
-//
-//    @Autowired
-//    private INotificationDetailService notificationDetailService;
 
 
     @GetMapping
@@ -54,30 +45,8 @@ public class ReplyController {
     @PostMapping
     public ResponseEntity<Reply> createComment(@RequestBody ReplyForm replyForm) {
         Reply reply = new Reply(replyForm.getText(),replyForm.getComment(),replyForm.getUser(),replyForm.getProfile(), new Date(), 0L, 0L);
-//        NotificationDetail notificationDetail = new NotificationDetail(new StatusNotification(4L), replyForm.getComment());
-//        notificationDetailService.save(notificationDetail);
+
         return new ResponseEntity<>(replyService.save(reply), HttpStatus.CREATED);
     }
-//
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<Comment> editComment(@PathVariable Long id, @RequestBody Comment comment) {
-//        Optional<Comment> commentOptional = commentService.findById(id);
-//        if (!commentOptional.isPresent()) {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//        comment.setId(id);
-//        return new ResponseEntity<>(commentService.save(comment), HttpStatus.OK);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Comment> deleteComment(@PathVariable Long id) {
-//        Optional<Comment> commentOptional = commentService.findById(id);
-//        if (!commentOptional.isPresent()) {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//        commentService.removeById(id);
-//        return new ResponseEntity<>(commentOptional.get(), HttpStatus.OK);
-//    }
 
 }
